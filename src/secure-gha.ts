4110d45f08deb74169826120a251b20f22f6a7c6
Rajat Jindal <rajatjindal83@gmail.com>
2024-07-21 07:18:03 +0530

    first try
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

---
+++ src/secure-gha.ts
@@ -0,0 +1,11 @@
+import * as core from '@actions/core'
+
+async function run(): Promise<void> {
+  try {
+    console.log("inside run")
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run()

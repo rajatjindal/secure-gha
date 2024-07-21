d523083960dd4b3f57bf6f887d9cc96bf10449fd
Rajat Jindal <rajatjindal83@gmail.com>
2024-07-21 21:56:51 +0530

    forbidden keyworkds
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- test.sh
+++ test.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 gh pr diff 1 -R rajatjindal/secure-gha --name-only > changed-files.txt
+echo "changed files:"
 cat changed-files.txt
-cat config/forbidden-files.txt
 echo
 echo
-for forbidden_file in $(cat config/forbidden-files.txt); do
-	echo "file is $forbidden_file"
-	grep $forbidden_file changed-files.txt
-	if [[ $? -eq "0" ]]; then
-		echo "forbidden file $forbidden_file changed"
-		## cancel all runs for this branch
-		for id in $(gh run list -b check-pr -R rajatjindal/secure-gha --json databaseId | jq '.[].databaseId'); do gh run cancel $id; done;
-	fi
 
-	echo
-	echo
-done;
+echo "safelisted files"
+cat config/safelisted-files.txt
+echo
+echo
+for allowed_file in $(cat config/safelisted-files.txt); do
+	sed '/$allowed_file/!d' changed-files.txt
+done
+echo
+echo
+echo "changed files after ignoring safelisted files"
+cat changed-files.txt
+echo
+echo
+for changed_file in $(cat changed-files.txt); do
+	echo $changed_file
+done

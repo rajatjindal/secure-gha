00d4a0f8a0c972e5727a783beec9686d7baad94e
Rajat Jindal <rajatjindal83@gmail.com>
2024-07-21 22:02:20 +0530

    add forbidden keywords to changed files
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+release=v0.1.0
+
 gh pr diff 1 -R rajatjindal/secure-gha --name-only > changed-files.txt
 echo "changed files:"
 cat changed-files.txt
@@ -10,15 +12,20 @@ echo "safelisted files"
 cat config/safelisted-files.txt
 echo
 echo
-for allowed_file in $(cat config/safelisted-files.txt); do
-	sed '/$allowed_file/!d' changed-files.txt
-done
-echo
-echo
-echo "changed files after ignoring safelisted files"
-cat changed-files.txt
-echo
-echo
 for changed_file in $(cat changed-files.txt); do
-	echo $changed_file
+	grep $changed_file config/safelisted-files.txt 2>&1 >/dev/null
+	if [[ $? -eq "0" ]]; then
+		echo "file $changed_file is safelisted for keywords check"
+		continue
+	fi
+
+	for keyword in $(cat config/forbidden-keywords.txt); do
+		echo "checking for keyword $keyword"
+		grep $keyword $changed_file
+		if [[ $? -eq "0" ]]; then
+			echo "$changed_file has forbidden keywords"
+			## cancel all runs for this branch
+			for id in $(gh run list -b check-pr -R rajatjindal/secure-gha --json databaseId | jq '.[].databaseId'); do gh run cancel $id; done;
+		fi
+	done
 done

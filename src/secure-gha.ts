7272a96b2496b2e6e1cc23178fb18dbf3d75c37a
Rajat Jindal <rajatjindal83@gmail.com>
2024-07-21 10:17:51 +0530

    print pr
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- src/secure-gha.ts
+++ src/secure-gha.ts
@@ -1,8 +1,9 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    console.log("inside run")
+    console.log("inside run ", github.context.payload.pull_request?.number)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
